728bfbcf07b1a3b82433ff24f71642185e3aea61
Evans Balcazar Veizaga <evansbv@gmail.com>
2024-09-01 00:35:50 -0400

    index-mejorado-css

--- resources/views/magazines/index.blade.php
+++ resources/views/magazines/index.blade.php
@@ -1,3 +1,4 @@
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -9,36 +10,53 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __($title) }}
+                    <h1>{{ __($title) }}</h1>
                         @if(Auth::user()->isAdmin())
                         <p>
-                          <a href="{{route('magazines.create')}}" >{{ __('*New*') }}</a>
+                          <a class="btn btn-primary" href="{{route('magazines.create')}}" >{{ __('Nuevo') }}</a>
                         </p>
                         @endif
-                        <ul>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>AÑO-MES</th>
+                                    <th>DESCRIPCION</th>
+                                    <th>ACCIONES</th>
+                                </tr>
+                            </thead> 
+                            <tbody>   
                             @foreach ($magazines as $magazine)
-                                <li>{{ $magazine->ano }}  {{ $magazine->mes}}  {{ $magazine->titulo}}
+                            <tr>
+                                <td>
+                                {{ $magazine->ano }} - {{ $magazine->mes}}
+                                </td>
+                                <td>
+                                  {{ $magazine->titulo}}
+                                </td>
+                                <td>
                                 @if(!$magazine->trashed())
-                                    <a href="{{route('magazines.show',[$magazine->id])}}"  >{{ __('*Details*') }}</a>
+                                    <a class="btn btn-success" href="{{route('magazines.show',[$magazine->id])}}"  >{{ __('Detalles') }}</a>
                                 @endif
                                 @if(Auth::user()->isAdmin())
                                     @if(!$magazine->trashed())    
-                                       <a href="{{route('magazines.edit',[$magazine->id])}}"  >{{ __('*Edit*') }}</a>
-                                       form action="{{  route('magazines.destroy',[$magazine->id]) }} " method="post">
+                                       <a class="btn btn-warning" href="{{route('magazines.edit',[$magazine->id])}}"  >{{ __('Editar') }}</a>
+                                       <form class="btn btn-danger" action="{{  route('magazines.destroy',[$magazine->id]) }} " method="post">
                                             {{ csrf_field() }}
             				                {{ method_field('DELETE') }}
                                             <button type="submit" >Borrar</button>
                                        </form>
                                        @else
-                                          <form action="{{  route('magazines.restore',[$magazine->id]) }} " method="post">
+                                          <form  action="{{  route('magazines.restore',[$magazine->id]) }} " method="post">
                                                {{ csrf_field() }}
-                                               <button type="submit" >Restaurar</button>
+                                               <button class="btn btn-danger" type="submit" >Restaurar</button>
                                           </form>
                                        @endif
                                     @endif
-                                </li>
+                                </td>
+                            </tr>
                             @endforeach
-                        </ul>
+                            </tbody>
+                        </table>
                         {{$magazines->links()}}
                 </div>
             </div>
